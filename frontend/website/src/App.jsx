b04dc42c1128ecf4b117a5c24455fdf19aa21536
jalzoren <flavierlaurence01@gmail.com>
2025-11-02 17:10:47 +0800

    OMG KA

--- frontend/website/src/App.jsx
+++ frontend/website/src/App.jsx
@@ -1,16 +1,49 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-export default function ScrollToTop() {
-  const { pathname } = useLocation();
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Doctors from "./pages/Doctors";
+import News from "./pages/News";
+import Announcements from "./pages/Announcements";
+import Careers from "./pages/Careers";
+import Contact from "./pages/Contact";
+import ComponentNews from "./components/ComponentNews";
 
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth", 
-    });
-  }, [pathname]);
+import "./App.css";
+import ComponentAnnouncement from "./components/ComponentAnnouncement";
+import ComponentCareers from "./components/ComponentCareers";
+import ScrollToTop from "./components/ScrollToTop";
 
-  return null;
+function App() {
+  return (
+    <Router>
+                  <ScrollToTop />
+
+      <Navbar />
+
+
+      {/* Page Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/announcements" element={<Announcements />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Full News Article Page (Dynamic ID) */}
+        <Route path="/news/:id" element={<ComponentNews />} />
+                <Route path="/announcements/:id" element={<ComponentAnnouncement />} />
+                <Route path="/careers/:id" element={<ComponentCareers />} />
+
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
+
+export default App;
